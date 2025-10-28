22c98dcfbf8b71ca7b5b19500a848452b2880f65
Sawadogo <payhuk02@gmail.com>
2025-10-28 10:47:29 +0000

    FIX: Import Input dans PhysicalVariantsBuilder

--- src/components/products/create/physical/PhysicalVariantsBuilder.tsx
+++ src/components/products/create/physical/PhysicalVariantsBuilder.tsx
@@ -5,7 +5,7 @@
 
 import { useState } from 'react';
 import { Button } from '@/components/ui/button';
-import { Input } from '@/components/ui/label';
+import { Input } from '@/components/ui/input';
 import { Label } from '@/components/ui/label';
 import { Switch } from '@/components/ui/switch';
 import { Card, CardContent, CardDescription, CardHeader, CardTitle } from '@/components/ui/card';
