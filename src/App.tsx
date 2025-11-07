c4320f882bf58802827c5d31fed728b6e29fdb21
Sawadogo <payhuk02@gmail.com>
2025-11-07 08:07:22 +0000

    fix: Corriger validation DSN Sentry et ajouter future flags React Router

--- src/App.tsx
+++ src/App.tsx
@@ -531,7 +531,12 @@ const App = () => (
     <TooltipProvider>
       <Toaster />
       <Sonner />
-      <BrowserRouter>
+      <BrowserRouter
+        future={{
+          v7_startTransition: true,
+          v7_relativeSplatPath: true,
+        }}
+      >
         <AuthProvider>
           <AppContent />
         </AuthProvider>
